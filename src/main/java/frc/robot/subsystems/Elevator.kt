f17aa44e35c7de1df675e124a2d794fd79ffc21a
Augustine-Doan <139661730+Augustine-Doan@users.noreply.github.com>
2025-09-27 10:18:41 -0700

    fix(elevator): fixed motion magic pingu in simple configs

--- src/main/java/frc/robot/subsystems/Elevator.kt
+++ src/main/java/frc/robot/subsystems/Elevator.kt
@@ -10,10 +10,10 @@ import com.ctre.phoenix6.controls.VoltageOut
 import com.ctre.phoenix6.hardware.TalonFX
 import com.ctre.phoenix6.signals.InvertedValue
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import frc.robot.utils.ElevatorParameters.ELEVATOR_MAGIC_PINGU
 import frc.robot.utils.ElevatorParameters.ELEVATOR_PINGU
 import frc.robot.utils.ElevatorParameters.ELEVATOR_SOFT_LIMIT_DOWN
 import frc.robot.utils.ElevatorParameters.ELEVATOR_SOFT_LIMIT_UP
-import frc.robot.utils.PivotParameters.PIVOT_MAGIC_PINGU
 import frc.robot.utils.emu.ElevatorState
 import xyz.malefic.frc.extension.configureWithDefaults
 import xyz.malefic.frc.pingu.AlertPingu.add
@@ -35,7 +35,7 @@ object Elevator : SubsystemBase() {
             InvertedValue.CounterClockwise_Positive,
             limitThresholds = ELEVATOR_SOFT_LIMIT_UP to ELEVATOR_SOFT_LIMIT_DOWN,
             dutyCycleNeutralDeadband = 0.1,
-            motionMagicPingu = PIVOT_MAGIC_PINGU,
+            motionMagicPingu = ELEVATOR_MAGIC_PINGU,
         )
         velocityRequest = VelocityTorqueCurrentFOC(0.0)
         posRequest = PositionDutyCycle(0.0)
