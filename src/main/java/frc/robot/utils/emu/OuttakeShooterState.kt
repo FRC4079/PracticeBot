f01438e8df2e35f2a98dca79f37dc6a1a5ec0512
Augustine-Doan <139661730+Augustine-Doan@users.noreply.github.com>
2025-09-27 08:39:30 -0700

    chore(outtake shooter state): dw about this commit it does nothing really

--- src/main/java/frc/robot/utils/emu/OuttakeShooterState.kt
+++ src/main/java/frc/robot/utils/emu/OuttakeShooterState.kt
@@ -3,5 +3,6 @@ package frc.robot.utils.emu
 enum class OuttakeShooterState(
     @JvmField val pos: Double,
 ) {
+    // om pls see this, it is definitely ok to make an enum with one value
     FORWARD(1.0),
 }
